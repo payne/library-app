d747762c1fa0fd678eab4539aac861c503cf5e85
Matt Payne <MPayne@LinkedIn.com>
2016-09-24 22:25:57 -0500

    Homework step 1

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: Ember.computed.not('isValid'),
+
+    emailAddress: '',
+    message: '',
+
+    actualEmailAddress: Ember.computed('emailAddress', function() { 
+      console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+    }),
+
+    emailAddressChanged: Ember.observer('emailAddress', function() { 
+      console.log('observer is called', this.get('emailAddress')); 
+    }),
+    actions: {
+
+      saveInvitation() {
+        alert(`Sending of the following email is in progress: ${this.get('emailAddress')}`);
+        this.set('responseMessage', `Thank you! We've just sent your email from: ${this.get('emailAddress')}`);
+        this.set('emailAddress', '');
+      }
+    }
 });
